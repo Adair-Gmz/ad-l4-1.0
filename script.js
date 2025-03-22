3b5e0003d4371b3569146d0e8645f9fbd5513db0
Edgar-s-J <palma.tedgar@gmail.com>
2025-03-21 19:31:32 -0600

    Portafolio

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-//Evento que recibirá la información del formulario cuando se active el evento submit cuando se presione el botón
-document.getElementById('formulario').addEventListener('submit', function(event) {
-    // Prevenie el evento por defecto del formulario de redireccionar
+// Agregar evento de scroll para cambiar el color del encabezado
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.background = '#444';
+    } else {
+        header.style.background = '#333';
+    }
+});
+
+// Agregar funcionalidad al formulario de contacto
+document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault();
-    // Obtenemos la información del formulario de cada input con su id
-    const nombre = document.getElementById('userName').value;
-    const email = document.getElementById('userEmail').value;
-    const mensaje = document.getElementById('userMessage').value;
-    //Comprobar que se haya recibido la información
-    if(nombre!=""&&email!=""&&mensaje!="")
-    {
-        //En caso de si recibir información, se muestra la alerta de envío exitoso
-        alert(nombre+" su mensaje ha sido enviado exitosamente");
-        document.getElementById('userName').value="";
-        document.getElementById('userEmail').value="";
-        document.getElementById('userMessage').value="";
-    }
-    else 
-    {
-        //En caso de que por error se reciba información en blanco, se solicita llenar los campos
-        //Probablemente este error no ocurra porque se aplico required a todos los input del formulario
-        alert("Llene los campos");
-    }
+    alert('Mensaje enviado con éxito!');
 });
